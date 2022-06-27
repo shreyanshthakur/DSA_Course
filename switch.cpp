c751e5f4e5c67c3bd44063f57c6ee321a9e2d6cc
shreyanshthakur <ranu10072000@gmail.com>
2022-06-27 18:56:13 +0530

    added sumofarray

---
+++ switch.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main () {
+    cout << "Enter amount : " << endl;
+    int n;
+    cin >> n;
+    switch (n) {
+        case 1  : 
+    }
+
+    return 0;
+}
