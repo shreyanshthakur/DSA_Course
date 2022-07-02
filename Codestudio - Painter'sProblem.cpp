10f6f8fd11f9cf2cea4df75cf2f47f51403e8586
shreyanshthakur <ranu10072000@gmail.com>
2022-07-02 20:05:24 +0530

    commit on 2 July

---
+++ Codestudio - Painter'sProblem.cpp
@@ -0,0 +1,46 @@
+bool isPossible (vector<int> &arr, int n, int m, int mid) {
+    int pageSum = 0;
+    int studentCount = 1;
+    for (int i = 0; i < n; i++) {
+        if (pageSum + arr[i] <= mid) {
+            pageSum += arr[i];
+        }
+        else {
+            studentCount++;
+            if (studentCount > m || arr[i] > mid) {
+                return false;
+            }
+            pageSum = arr[i];
+        }
+    }
+    return true;
+}
+
+int bookAllocation (vector<int> &arr, int m, int n) {
+    int s = 0;
+    int sum = 0;
+    for (int i = 0; i < n; i++) 
+        sum += arr[i];
+    int e = sum;
+    int ans = -1;
+    int mid = s + (e-s)/2;
+    while (s <= e) {
+        if (isPossible(arr, n, m, mid)) {
+            ans = mid;
+            e = mid - 1;
+        }
+        else {
+            s = mid + 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int findLargestMinDistance(vector<int> &boards, int k)
+{
+    //    Write your code here.
+    
+    int ans = bookAllocation(boards, k, boards.size());
+    return ans;
+}
