813756376b445ed7fa04d23a1bd0c0abceb2725e
shreyanshthakur <ranu10072000@gmail.com>
2022-07-04 21:13:30 +0530

    commit on 4 July

---
+++ Codestudio - SumOfTwoArrays.cpp
@@ -0,0 +1,47 @@
+vector<int> reverse(vector<int> ans) {
+    int s = 0;
+    int e = ans.size() - 1;
+    while (s < e) {
+        swap(ans[s++], ans[e--]);
+    }
+    return ans;
+}
+vector<int> findArraySum(vector<int>&a, int n, vector<int>&b, int m) {
+	// Write your code here.
+    vector<int> ans;
+    int i = n - 1;
+    int j = m - 1;
+    int carry = 0;
+    int sum = 0;
+    while (i >= 0 && j >= 0) {
+        sum = a[i] + b[j] + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+        i--;
+        j--;
+    }
+    //first case
+    while (i >= 0) {
+        sum = a[i] + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+        i--;
+    }
+    //second case
+    while (j >= 0) {
+        sum = b[j] + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+        j--;
+    }
+    while (carry != 0) {
+        sum = b[j] + carry;
+        carry = sum / 10;
+        sum = sum % 10;
+        ans.push_back(sum);
+    }
+    return reverse(ans);
+}
