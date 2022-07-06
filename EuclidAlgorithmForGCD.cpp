678cb8d290d635320adcecd42ae60d5148028637
shreyanshthakur <ranu10072000@gmail.com>
2022-07-06 18:38:53 +0530

    commit on 6 July

--- EuclidAlgorithmForGCD.cpp
+++ EuclidAlgorithmForGCD.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
+//Euclid Algorithm to find Greatest Common Divisor
+int gcd (int a, int b) {
+    if(a==0)
+        return b;
+    if(b==0)
+        return a;
+    while (a != b) {
+        if (a < b) {
+            b = b - a;
+        }
+        else {
+            a = a - b;
+        }
+    }
+    return a;
+}
 int main () {
-    cout << "Namaste Duniya";
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b);
     return 0;
 }
