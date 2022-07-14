10fca7c78a629e7b449ae98d983b5b2c4def674b
shreyanshthakur <ranu10072000@gmail.com>
2022-07-14 14:59:39 +0530

    commit on 14 July

---
+++ OOPs/class.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+class Hero {
+    int health;
+};
+int main () {
+    //object creation
+    Hero shaktimaan;
+    cout << "Size : " <<sizeof(shaktimaan
+    ) << endl;
+    return 0;
+}
