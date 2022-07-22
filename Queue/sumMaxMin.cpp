8c35af0cbce1f8dc79013a00e20ef3c7b9a38730
shreyanshthakur <ranu10072000@gmail.com>
2022-07-22 19:32:04 +0530

    added driver code

--- Queue/sumMaxMin.cpp
+++ Queue/sumMaxMin.cpp
@@ -2,11 +2,13 @@
 #include<queue>
 using namespace std;
 
-int solve (int *arr, int k) {
+int solve (int *arr, int n, int k) {
     deque<int> maxi(k);
     deque<int> mini(k);
-
+    
+    //Addition of first k size window
     for (int i = 0; i < k; i++) {
+
         while (!maxi.empty() && arr[maxi.back()] <= arr[i]) {
             maxi.pop_back();
         }
@@ -17,8 +19,11 @@ int solve (int *arr, int k) {
         mini.push_back(i);
     }
     int ans = 0;
+
+    ans += arr[maxi.front()] + arr[mini.front()];
+    //remaining window ko process karlo
     for(int i = k; i < n; i++) {
-        ans += arr[maxi.front()] + arr[mini.front()];
+
         //next window
 
         //removal
@@ -28,6 +33,7 @@ int solve (int *arr, int k) {
         while(!mini.empty() && i - mini.front() >= k) {
             mini.pop_front();
         }
+
         //addition
         while (!maxi.empty() && arr[maxi.back()] <= arr[i]) {
             maxi.pop_back();
@@ -35,13 +41,18 @@ int solve (int *arr, int k) {
         while (!mini.empty() && arr[mini.back()] >= arr[i]) {
             mini.pop_back();
         }
+
         maxi.push_back(i);
         mini.push_back(i);
+
+        ans += arr[maxi.front()] + arr[mini.front()];
     }
-    ans += arr[maxi.front()] + arr[mini.front()];
     return ans;
 }
 
 int main() {
+    int arr[7] = {2, 5, -1, 7, -3, -1, -2};
+    int k = 4;
+    cout << solve(arr, 7, k) << endl;
     return 0;
 }
