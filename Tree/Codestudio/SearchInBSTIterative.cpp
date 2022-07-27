b7b869a39712216c60db57a983f0c25b3e2e0646
shreyanshthakur <ranu10072000@gmail.com>
2022-07-27 19:56:34 +0530

    commit on 27 July

---
+++ Tree/Codestudio/SearchInBSTIterative.cpp
@@ -0,0 +1,33 @@
+/*
+    Following is the Binary Tree node structure:
+
+    class BinaryTreeNode {
+    public : 
+        T data;
+        BinaryTreeNode<T> *left;
+        BinaryTreeNode<T> *right;
+
+        BinaryTreeNode(T data) {
+            this -> data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+*/
+
+bool searchInBST(BinaryTreeNode<int> *root, int x) {
+    BinaryTreeNode<int> *temp = root;
+    
+    while (temp != NULL) {
+        if (temp->data == x) {
+            return true;
+        }
+        if (x < temp->data) {
+            temp = temp->left;
+        }
+        else {
+            temp = temp->right;
+        }
+    }
+    return false;
+}
