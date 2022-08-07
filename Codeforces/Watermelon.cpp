661357a5a7a39051f9931fd94b2198191c9cca1b
shreyanshthakur <ranu10072000@gmail.com>
2022-08-07 12:46:01 +0530

    commit on 7 August

---
+++ Codeforces/Watermelon.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main () {
+    int w;
+    cin >> w;
+    if (w > 2) 
+        cout << ( (w%2 == 0) ? "YES" : "NO" );
+    else
+        cout << "NO";
+    return 0;
+}
